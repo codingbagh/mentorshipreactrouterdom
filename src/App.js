4d9961332d7316951ef0d21dce88b346c9b417f3
Boateng Oduro Kelvin <83387330+codingbagh@users.noreply.github.com>
2022-12-13 16:23:37 +0000

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 
+import { BrowserRouter as Kelvin,Route,Routes,Link } from "react-router-dom";
+import About from "./Routes/About";
+import Home from "./Routes/Home";
+
 const App = ()=>{
 return(
-<div>App</div>
+<Kelvin>
+<div>
+    <ul style={{display:"flex",gap:"10px"}}>
+      <Link to="/">Home</Link>
+      <Link to="/About">About</Link>
+    </ul>
+</div>
+
+<Routes>
+<Route  path="/" element={<Home/>}/>
+<Route path="/About" element={<About/>}/>
+
+</Routes>
+</Kelvin>
+
 )
 
 }
